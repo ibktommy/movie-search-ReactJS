b4f9e02b8389f7d7e23056b1ef89ff773c2b08a3
ibktommy <atomquartz@gmail.com>
2022-09-15 11:48:42 +0100

    Add APIKEY to the env file

--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
