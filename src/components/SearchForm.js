9773a3fab6dca4e6036263f54558c97179d79409
ibktommy <atomquartz@gmail.com>
2022-09-15 13:48:34 +0100

    Adding elements and styles to the searchForm component, setting value of the form-input to the searchTerm-state-value

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
-  return <h2>search component</h2>
+  // Grabbing Values from the App-Global-Context
+  const { isLoading, searchTerm, setSearchTerm } = useGlobalContext()
+
+  return (
+    <form className="search-form">
+      <h2>search movies</h2>
+      <input type="text" className='form-input' value={searchTerm} onChange={(e) => e.target.value} />
+    </form>
+  )
 }
 
 export default SearchForm
