b2c4da6134f209e2ad980ec36f4fa973c6c1fbf3
ibktommy <atomquartz@gmail.com>
2022-09-15 13:55:50 +0100

    Display error message based on the condition if error-state is true/false

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,12 +3,13 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   // Grabbing Values from the App-Global-Context
-  const { isLoading, searchTerm, setSearchTerm } = useGlobalContext()
+  const { error, searchTerm, setSearchTerm } = useGlobalContext()
 
   return (
     <form className="search-form">
       <h2>search movies</h2>
       <input type="text" className='form-input' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      {error.show && <div className='error'>{error.message}</div>}
     </form>
   )
 }
