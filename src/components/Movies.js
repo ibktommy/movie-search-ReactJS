494f2d64a6b9bab4da427a10716f12cba0af309d
ibktommy <atomquartz@gmail.com>
2022-09-15 13:40:55 +0100

    Add a default image for each movie in case image is not available in the API

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 import { Link } from 'react-router-dom'
 
-// const url =
-//   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+const moviePoster =
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const Movies = () => {
   // Grabbing Values from the App-Global-Context
@@ -22,7 +22,7 @@ const Movies = () => {
           return (
             <Link to={`/movies/${id}`} key={id} className='movie'>
               <article>
-                <img src={poster} alt={title} />
+                <img src={poster === 'N/A' ? moviePoster : poster} alt={title} />
                 <div className="movie-info">
                   <h4 className="title">{title}</h4>
                   <p>{year}</p>
