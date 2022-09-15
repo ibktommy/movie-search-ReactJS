310f414fd8bb9835f321a92436a6e81198b250ff
ibktommy <atomquartz@gmail.com>
2022-09-15 15:28:09 +0100

    Add elements to the SIngle movies based on the data from the movieDetail-arry

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { apiUrl } from './context'
 
+const moviePoster =
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+
 const SingleMovie = () => {
   // Setting COmponent State
   const [movieDetail, setMovieDetail] = useState({})
@@ -22,8 +25,6 @@ const SingleMovie = () => {
       setMovieDetail(data)
       setIsLoading(false)
     }
-
-    console.log(data)
   }
 
   // UseEfect HooK
@@ -31,7 +32,34 @@ const SingleMovie = () => {
     fetchMovieDetails( `${apiUrl}&i=${id}`)
   }, [id])
 
-  return <h2>single movie</h2>
+  if (isLoading) {
+    return <div className="loading"></div>
+  }
+
+  if (error.show) {
+    <return className="page-error">
+      <h1>{error.message}</h1>
+      <Link to='/'>
+        back to movies
+      </Link>
+    </return>
+  }
+
+  // Destructuring properties from the movieDetail-Object
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movieDetail
+  return (
+    <section className="single-movie">
+      <img src={poster === 'N/A' ? moviePoster : poster} alt={title} />
+      <div className="singlemovie-info">
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to='/' className='btn'>
+          back to movies
+        </Link>
+      </div>
+    </section>
+  )
 
 }
 
